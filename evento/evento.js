2955479e249c7417c5e87dd9763fbf113a7405ec
Gerson Ramirez Paez <Gerson.Ramirez@interedes.local>
2025-05-30 16:14:07 -0500

    resuelto

--- evento/evento.js
+++ evento/evento.js
@@ -1,29 +1,38 @@
- const btn = document.querySelector("button");
+const btn = document.querySelector("button");
 
- function random(number){
-     return Math.floor(Math.random() * (number +1));
- }
- btn.addEventListener("click", () =>{
-     const rndCol =`rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-     document.body.style.backgroundColor = rndCol;
-        btn.style.position = 'relative';
-        btn.style.top ='50px';
-        btn.style.left = '100px';
-
- })
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+btn.addEventListener("click", () => {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+  btn.style.position = "absolute";
+  btn.style.top = random(100) + "px";
+  btn.style.left = random(100) + "px";
+});
 
 const texto = document.querySelector('input[type="text"]');
 
-texto.addEventListener("mouseover", (Event)=>{
-    Event.target.style.backgroundColor = "pink";
-    
-})
+texto.addEventListener("mouseover", (Event) => {
+  Event.target.style.backgroundColor = "pink";
+});
 
-texto.addEventListener("mousout", (Event)=> {
-    Event.target.style.backgroundColor = "";
-})
+texto.addEventListener("mousout", (Event) => {
+  Event.target.style.backgroundColor = "";
+});
 
+let clickCount = 0;
+const button = document.getElementById("myButton");
+const countDisplay = document.getElementById("clickCount");
 
-const calcula = document.querySelector("1")
+button.addEventListener("click", () => {
+  clickCount++;
+  countDisplay.textContent = "Clicks: " + clickCount;
+});
 
-
+function calculaRaiz() {
+  const numero = document.getElementById("numero").value;
+  const raiz = Math.sqrt(numero);
+  document.getElementById("resultado").textContent =
+    " la raiz cuadrada de " + numero + " es : " + raiz;
+}
